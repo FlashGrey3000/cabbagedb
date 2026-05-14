0c486940338006f6cb14102349e5e04ccd900e05
flashgrey <aaftabkhan2005@gmail.com>
2026-05-15 00:17:20 +0530

    feat(cli): added ansi log and ascii logo

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "cli.c"
+
+int main() {
+    printf("---- <main> ----\n");
+    printf("main\n");
+    print_logo();
+    printf("---- <main> ----\n");
+
+    return 0;
+}
